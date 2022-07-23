a8c5c7f2ccb03b8e6249ff9c42c7af9cb4c0c970
Paul Chibueze <paulpadro03@gmail.com>
2022-07-23 20:12:59 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'esd6568elf';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-883xf7nm.us.auth0.com',
+  clientId: 'xK8l9Q61t2viOmhMRnEiqjL7tM60hcf8',
+  callbackUrl: 'http://localhost:3000/callback'
+};
